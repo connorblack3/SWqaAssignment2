74d64f468886da572d58697bbddff9143889db48
connorblack3 <65261129+connorblack3@users.noreply.github.com>
2022-04-12 19:36:29 -0500

    Update BMICalculator.py

--- BMICalculator.py
+++ BMICalculator.py
@@ -25,6 +25,26 @@ def calcBMI (height, weight):
 
     return BMI
 
+def BMI (feet, inches, weight):
+    
+    height = 12 * float(feet) + float(inches)
+    
+    newWeight = float(weight) * 0.45
+    newHeight = float(height) * 0.025
+    
+    newHeight = pow(newHeight, 2)
+    
+    bmi = newWeight / newHeight
+    bmi = truncate(bmi,1)
+    if (18.5 > float(bmi)):
+        return "Underweight"
+    if (18.5 <= float(bmi) and float(bmi) <=24.9):
+        return "Normal Weight"
+    if (25 <= float(bmi) and float(bmi) <= 29.9):
+        return "Overweight"
+    if (30 <= float(bmi)):
+        return "Obese"
+
 def detBMIcatagory (BMI):
 
     if (18.5 > float(BMI)):
