e5c17a86d9fc5037ba387e6985d10fe4dc4e8543
connorblack3 <blackmail.connor@gmail.com>
2022-04-13 02:59:38 +0000

    Flast integration

--- bmi.py
+++ bmi.py
@@ -1,32 +1,20 @@
 import math
-from flask import Flask
-from markupsafe import escape
-
-
-def truncate(number, digits) -> float:
-    stepper = 10.0 ** digits
-    return math.trunc(stepper * number) / stepper
+from flask import Flask, request
+from BMICalculator import BMI
 
 
 app = Flask(__name__)
 
-@app.route("/bmi/<fliat:feet>,<float:inches>,<float:weight>")
-def BMI (feet, inches, weight):
-    
-    height = 12 * float(feet) + float(inches)
-    
-    newWeight = float(weight) * 0.45
-    newHeight = float(height) * 0.025
-    
-    newHeight = pow(newHeight, 2)
-    
-    bmi = newWeight / newHeight
-    bmi = truncate(bmi,1)
-    if (18.5 > float(bmi)):
-        return "Underweight"
-    if (18.5 <= float(bmi) and float(bmi) <=24.9):
-        return "Normal Weight"
-    if (25 <= float(bmi) and float(bmi) <= 29.9):
-        return "Overweight"
-    if (30 <= float(bmi)):
-        return "Obese"
+@app.route("/", methods=["GET"])
+def hello_world():
+
+
+    feet = request.values.get('feet', default="1", type = float)
+    print(feet)
+    inch = request.values.get('inch', default = "1", type = float)
+    print(inch)
+    weight = request.values.get('weight', default = "200", type = float)
+    print(weight)
+
+    return_value = BMI(feet, inch, weight)
+    return return_value
